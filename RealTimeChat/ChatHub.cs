836cf34c1b5a36b3fb0d8c7f808ac3529749852d
satyasn01 <satyasn07@gmail.com>
2024-09-30 03:02:17 +0530

    Add project files.

---
+++ RealTimeChat/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealTimeChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Broadcast message to all clients
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ RealTimeChat/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR;
+
+[assembly: OwinStartup(typeof(RealTimeChat.Startup))]
+namespace RealTimeChat
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Configure SignalR
+            app.MapSignalR();
+        }
+    }
+}
+
